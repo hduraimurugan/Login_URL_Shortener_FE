84a69bea54e82d005d1f9bc246bfa71c33291573
Duraimurugan-H <hduraimurugan@gmail.com>
2024-10-10 19:54:21 +0530

    api url changed

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
-const API_URL ="https://login-backend-2aaw.onrender.com/api/auth";
+const API_URL ="https://login-auth-fullstack.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 
